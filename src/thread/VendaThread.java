9e8d1e18f2210d20abcecc91d9f07fa897312d85
joaomarcossouza-FAC-UC <joaomarcos.souza@ucsal.edu.br>
2026-06-01 13:15:35 -0300

    feat: thread VendaThread.java - utilizando o Runnable para fazer o processamento dos objetos Venda.java ao mesmo tempo

---
+++ src/thread/VendaThread.java
@@ -0,0 +1,25 @@
+package thread;
+
+import service.VendaProcessos;
+import java.util.List;
+import model.Venda;
+
+public class VendaThread implements Runnable{
+	private List<Venda> vendas;
+	private VendaProcessos processos;
+	
+	public VendaThread(List<Venda> vendas, VendaProcessos processos) {
+		this.processos = processos;
+		this.vendas = vendas;
+	}
+
+	@Override
+	public void run() {
+		System.err.println(Thread.currentThread().getName()+ " processando "+vendas.size()+ " vendas... ");
+		processos.processar(vendas);
+		System.out.println(Thread.currentThread().getName()+ "finalizado");
+		
+	}
+	
+	
+}
