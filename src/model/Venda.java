6c0f04467c4d6d5ff718d4d5b687190226b45d0e
joaomarcossouza-FAC-UC <joaomarcos.souza@ucsal.edu.br>
2026-06-01 11:16:22 -0300

    feat: model Venda.java com os atributos e Gets

---
+++ src/model/Venda.java
@@ -0,0 +1,77 @@
+package model;
+
+	public class Venda {
+		private int id;
+		private String produto;
+		private String categoria;
+		private double valor;
+		private int quantidade;
+		
+		
+		public Venda(int id,String produto,String categoria,double valor,int quantidade) {
+			this.categoria = categoria;
+			this.id = id;
+			this.produto = produto;
+			this.quantidade = quantidade;
+			this.valor = valor;
+		}
+
+
+		public int getId() {
+			return id;
+		}
+
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+
+		public String getProduto() {
+			return produto;
+		}
+
+
+		public void setProduto(String produto) {
+			this.produto = produto;
+		}
+
+
+		public String getCategoria() {
+			return categoria;
+		}
+
+
+		public void setCategoria(String categoria) {
+			this.categoria = categoria;
+		}
+
+
+		public double getValor() {
+			return valor;
+		}
+
+
+		public void setValor(double valor) {
+			this.valor = valor;
+		}
+
+
+		public int getQuantidade() {
+			return quantidade;
+		}
+
+
+		public void setQuantidade(int quantidade) {
+			this.quantidade = quantidade;
+		}
+
+
+		@Override
+		public String toString() {
+			return "Venda [id=" + id + ", produto=" + produto + ", categoria=" + categoria + ", valor=" + valor
+					+ ", quantidade=" + quantidade + "]";
+		}
+		
+		
+}
